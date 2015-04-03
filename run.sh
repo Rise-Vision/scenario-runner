f72538e08a18940f32f583afee833d99a3d2bdc2
tejohnso <mail@tyler-johnson.ca>
2015-04-02 22:04:02 -0400

    Keep failed logs

--- run.sh
+++ run.sh
@@ -17,10 +17,13 @@ do
     git clone $targetUrl;
   fi
 
-  cd $TARGET_DIR; git fetch origin; MERGERESULT=$(git merge origin/master -X theirs);
+  cd $TARGET_DIR;
+  git checkout master;
+  git fetch origin;
+  MERGERESULT=$(git merge origin/master -X theirs -m "merge");
 
   PASSFAIL=fail
-  if npm run e2e; then
+  if npm run e2e >E2E_OUTFILE 2>&1; then
     PASSFAIL=pass
   fi
 
@@ -30,6 +33,8 @@ do
   echo $PASSFAIL >> $TARGET_DIR.log
 
   if [ $PASSFAIL == "fail" ]; then
+    mv $TARGET_DIR/E2E_OUTFILE $TARGET_DIR/E2E_ERROR_$(date) || true
+    mv $TARGET_DIR/uncaught-exception.png $TARGET_DIR/uncaught-exception-$(date).png || true
     TOKEN=curl "http://metadata/computeMetadata/v1/instance/service-accounts/default/token?alt=text" \
     -H "Metadata-Flavor: Google" |grep access_token |awk '{print $2}'
     curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer $TOKEN" \
