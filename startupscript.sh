c45d28550389ae8e2c9057246d2be80409f0a949
tejohnso <mail@tyler-johnson.ca>
2015-04-16 10:00:07 -0400

    Startup corrections

--- startupscript.sh
+++ startupscript.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 if [ ! -f /swapfile ]; then
   sudo dd if=/dev/zero of=/swapfile bs=1024 count=4194304
   sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
+  sudo mkswap /swapfile
   sudo swapon -a
 fi
 
@@ -12,13 +13,6 @@ if ! which git; then
   sudo apt-get -y install git-core
   git config --global user.name "jenkins"
   git config --global user.email "jenkins@risevision.com"
-fi
-
-if ! gcloud auth list |grep 452091732215@developer.gserviceaccount.com; then
-  git clone git@github.com:Rise-Vision/private-keys.git
-  cd private-keys
-  gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file storage-server/rva-media-library-ce0d2bd78b54.json
-  cd ..
 fi
 
 if ! which node; then
@@ -58,4 +52,6 @@ if ! pidof Xvfb ; then
   Xvfb :10 -screen 0 1024x768x24 &
 fi
 
-crontab -u scenario-runner /home/scenario-runner/cronfile
+cd /home/scenario-runner
+git clone https://github.com/Rise-Vision/scenario-runner.git config
+crontab -u scenario-runner config/cronfile
