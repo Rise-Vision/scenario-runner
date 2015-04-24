6ef0cd302e4382cb3e7f26bd96406080851f4b6d
tejohnso <mail@tyler-johnson.ca>
2015-04-24 12:35:21 -0400

    Make sure bzip exists (for tar extractions)

--- startupscript.sh
+++ startupscript.sh
@@ -28,6 +28,10 @@ if ! which unzip; then
   sudo apt-get install -y unzip
 fi
 
+if ! which bzip2; then
+  sudo apt-get install -y bzip2
+fi
+
 if ! which google-chrome; then
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg -i google-chrome-*.deb
